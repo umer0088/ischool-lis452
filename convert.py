beefa76f9c638ff2732659534484e584fa7acbd4
YBull <jweible@illinois.edu>
2017-03-09 16:15:19 -0600

    Modified convert.py to catch input data type exceptions and re-ask the user for a valid input.

--- convert.py
+++ convert.py
@@ -1,15 +1,22 @@
 # convert.py
 #   A program to convert Celsius temps to Fahrenheit
-# by: Susan Computewell
+#
+# J. Weible, based on example from John Zelle's 2nd edition Python textbook.
 
 def main():
+    """Interactive converter of temperatures."""
+    times_to_loop = False
+    while times_to_loop == False:
+        try:
+            times_to_loop = int(input("How many temperatures do you want to convert?"))
+        except ValueError:
+            print("Retry, by entering an integer number.")
+            times_to_loop = False
 
-    times_to_loop = int(input("How many temperatures do you want to convert?"))
     for i in range(times_to_loop):
         celsius = int(input("What is the Celsius temperature? "))
         fahrenheit = 9/5 * celsius + 32
         print("The temperature is", fahrenheit, "degrees Fahrenheit.")
-
 
 main()
 
