39a17e75c727cde448612c76bbbe37afcb656949
YBull <jweible@illinois.edu>
2017-03-10 10:33:33 -0600

    Changed to defaultdict data type and added comments.

--- wordfreq.py
+++ wordfreq.py
@@ -1,4 +1,10 @@
 # wordfreq.py
+#
+# Based on John Zelle's example from PPICS 2nd ed. Page 375.
+#
+# Slightly modified by J. Weible
+
+from collections import defaultdict
 
 def byFreq(pair):
     return pair[1]
@@ -16,9 +22,9 @@ def main():
     words = text.split()
 
     # construct a dictionary of word counts
-    counts = {}
+    counts = defaultdict(int)  # dict where non-existent keys get automatically assigned an integer 0
     for w in words:
-        counts[w] = counts.get(w,0) + 1
+        counts[w] += 1
 
     # output analysis of n most frequent words.
     n = eval(input("Output analysis of how many words? "))
@@ -29,4 +35,6 @@ def main():
         word, count = items[i]
         print("{0:<15}{1:>5}".format(word, count))
 
+# This "magic" line below will allow this file to be imported by another script
+# without automatically executing the functions contained in it:
 if __name__ == '__main__':  main()
